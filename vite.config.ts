bae21c23a359e4d75df222e60ff8065cd9aa915d
nojam2ya <jinjoo648@naver.com>
2024-08-09 01:20:35 +0900

    fix: env에 따라 base path 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,40 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: [
-      { find: '@src', replacement: resolve(__dirname, 'src') },
-      { find: '@style', replacement: resolve(__dirname, 'src/style') },
-      { find: '@routes', replacement: resolve(__dirname, 'src/routes') },
-      {
-        find: '@components',
-        replacement: resolve(__dirname, 'src/components'),
-      },
-    ],
-  },
-  base: '/coin/',
-});
+// export default defineConfig({
+//   plugins: [react(), tsconfigPaths()],
+//   resolve: {
+//     alias: [
+//       { find: '@src', replacement: resolve(__dirname, 'src') },
+//       { find: '@style', replacement: resolve(__dirname, 'src/style') },
+//       { find: '@routes', replacement: resolve(__dirname, 'src/routes') },
+//       {
+//         find: '@components',
+//         replacement: resolve(__dirname, 'src/components'),
+//       },
+//     ],
+//   },
+//   base: '/coin/',
+// });
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+      alias: [
+        { find: '@src', replacement: resolve(__dirname, 'src') },
+        { find: '@style', replacement: resolve(__dirname, 'src/style') },
+        { find: '@routes', replacement: resolve(__dirname, 'src/routes') },
+        {
+          find: '@components',
+          replacement: resolve(__dirname, 'src/components'),
+        },
+      ],
+    },
+    // base: '/coin/',
+    base: env.VITE_BASE_URL,
+  });
+};
